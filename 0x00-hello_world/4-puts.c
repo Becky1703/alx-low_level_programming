2eefdaa79885f1a129648e7d53288f06b7b7c9ae
Becky1703 <oluwabukunmia@gmail.com>
2022-12-08 10:51:16 -0800

    Added a new file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  * main - prints the phrase "Programming is like building a multilingual puzzle
  * Return: 0 if exited properly, otherwise non-zero
  */
-int main (void)
+int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0);
 }
